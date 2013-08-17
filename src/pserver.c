c11c3cc4a746bc409b0dfd659cbb081c0ad78961
Erik Hugne <erik.hugne@gmail.com>
2013-08-17 10:50:53 +0200

    pperf: sdt=c99, posix src, address parsing etc
    Signed-off-by: Erik Hugne <erik.hugne@gmail.com>

--- src/pserver.c
+++ src/pserver.c
@@ -4,26 +4,65 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/queue.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <netdb.h>
 #include <pthread.h>
 
+#define PSERVER_DEFAULT_LISTEN_PORT "4999"
+#define PSERVER_DEFAULT_LISTEN_ADDR "::"
+
 struct pserver_config {
-	struct addrinfo ai_control;
-	int control;
+	struct addrinfo ai_listen;
+	int sd_listen;
+	int sd_control;
 };
 
+struct pserver_thread {
+	pthread_t id;
+	int sd_control;
+	LIST_ENTRY(pserver_thread) next;
+};
 
-static const char *global_optargs = "DL:";
+LIST_HEAD(threads_head, pserver_thread) pserver_threads;
 
+static const char *global_optargs = "DL:p:v";
 
-static int scan_options(int argc, char *argv[], struct pserver_config *config)
+static int verbosity = 0;
+
+#define pr_info(fmt, ...) {if(verbosity >= 1) \
+				printf("info: " fmt, __VA_ARGS__);}
+
+static void create_listen_sa(char *hostname, char *port,
+			     struct pserver_config *cfg)
 {
-	int c, s;
-	struct addrinfo *ai_listen = NULL;
+	int s;
 	struct addrinfo hints = {0};
+	struct addrinfo *ai_listen;
+
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_flags = AI_NUMERICHOST;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	if ((s = getaddrinfo(hostname, port, &hints, &ai_listen)) != 0) {
+		perror("getaddrinfo");
+		printf("%s", gai_strerror(s));
+		exit(-1);
+	}
+	memcpy(&cfg->ai_listen, ai_listen, sizeof(struct addrinfo));
+	freeaddrinfo(ai_listen);
+
+}
+
+static int scan_options(int argc, char *argv[],
+			struct pserver_config *cfg)
+{
+	int c;
+	char *hostname	= PSERVER_DEFAULT_LISTEN_ADDR;
+	char *port	= PSERVER_DEFAULT_LISTEN_PORT;
+
 	while ((c = getopt(argc, argv, global_optargs)) != -1) {
 		switch (c) {
 		case '?':
@@ -32,52 +71,69 @@ static int scan_options(int argc, char *argv[], struct pserver_config *config)
 		case 'D':
 		/*TODO: daemonize code*/
 		break;
+		case 'v':
+		verbosity = 1;
+		break;
 		case 'L':
-		hints.ai_family = AF_UNSPEC;
-		hints.ai_flags = AI_NUMERICHOST;
-		if ((s = getaddrinfo(optarg, NULL, &hints, &ai_listen)) != 0) {
-			perror("getaddrinfo");
-			printf("%s", gai_strerror(s));
-			return -EINVAL;
-		}
-		memcpy(&config->ai_control, ai_listen, sizeof(struct addrinfo));
-		freeaddrinfo(ai_listen);
+			hostname = optarg;
+		break;
+		case 'p':
+			port = optarg;
 		break;
 		default:
 		return -EINVAL;
 		}
 	}
+	pr_info("pserver listening on %s port %s\n", hostname, port);
+	create_listen_sa(hostname, port, cfg);
 	return 0;
 }
 
 int setup_pserver(struct pserver_config *config)
 {
 	const char on = 1;
-	config->control = socket(config->ai_control.ai_family, SOCK_STREAM, 0);
-	setsockopt(config->control, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-	bind(config->control, (struct sockaddr*)&config->ai_control,
-	     config->ai_control.ai_addrlen);
-	/*TODO: error checking..*/
 
+	config->sd_listen = socket(config->ai_listen.ai_family,
+				 config->ai_listen.ai_socktype,
+				 config->ai_listen.ai_protocol);
+	setsockopt(config->sd_listen, SOL_SOCKET, SO_REUSEADDR, &on,
+		   sizeof(on));
+	bind(config->sd_listen, (struct sockaddr*)&config->ai_listen,
+	     config->ai_listen.ai_addrlen);
+	/*TODO: error checking..*/
+	listen(config->sd_listen, 10);
 	return 0;
+}
+
+void *pserver_threadfunc(void *threadid)
+{
+	/**/
+	pthread_exit(0);
 }
 
 void pserver_loop(struct pserver_config *config)
 {
-	fd_set fds;
-	int nfds;
-	
+	struct sockaddr_storage ss_peer;
+	socklen_t ss_len;
+	struct pserver_thread *thread;
+
 	while (1) {
-		/*FIXME: select(), spawn pthread slave etc...*/
-		FD_ZERO(&fds);
-		FD_SET(config->control, &fds);
-		if ((nfds = select(1, &fds, NULL, NULL, NULL)) < 0) {
-			perror("select");
+		ss_len = sizeof(struct sockaddr_storage);
+		memset(&ss_peer, 0, ss_len);
+		if ((config->sd_control = accept(config->sd_listen,
+		    (struct sockaddr*)&ss_peer, &ss_len)) == -1) {
+		     perror("accept");
+		     exit(-1);
+		}
+		/*This is wrong, we should fork() for each control
+		connection (pserver instance), and the client/server
+		negotiates how many threads the test should run*/
+		thread = malloc(sizeof(struct pserver_thread));
+		if (!pthread_create(&thread->id, NULL, pserver_threadfunc,
+		    NULL)) {
+			perror("pthread_create");
 			exit(-1);
 		}
-		
-
-
 	}
 }
 
@@ -86,6 +142,8 @@ int main(int argc, char *argv[])
 {
 
 	struct pserver_config config = {{0}};
+
+	LIST_INIT(&pserver_threads);
 	if (scan_options(argc, argv, &config))
 		return -1;
 
