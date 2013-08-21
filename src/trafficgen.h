1d93708dbf44394840173c676dfed5bb0f2532c9
Kristofer Hallin <khallin@accedian.com>
2013-08-21 10:18:31 +0200

    More packet headers
    Signed-off-by: Kristofer Hallin <khallin@accedian.com>

--- src/trafficgen.h
+++ src/trafficgen.h
@@ -16,6 +16,12 @@
 #include <net/if.h>
 #include <errno.h>
 
+#define UDP_SIZE (8)
+#define TCP_SIZE (20)
+#define IPV4_SIZE (20)
+#define IPv6_SIZE (40)
+#define IPv6_ADDRESS_SIZE (16)
+
 /*
  * UDP header
  */
@@ -25,10 +31,6 @@ struct udp_header {
         u_int16_t       len;
         u_int16_t       cksum;
 };
-
-typedef struct udp_header udp_hdr;
-
-#define UDP_SIZE (8)
 
 /*
  * TCP header
@@ -50,7 +52,69 @@ struct tcp_header {
         u_int16_t       urgptr;
 };
 
+/*
+ * IPv4 Header
+ */
+struct ip_header_v4 {
+#if defined(__LITTLE_ENDIAN__)
+        u_int8_t	hlen:4, ver:4;
+#else
+        u_int8_t	ver:4, hlen:4;
+#endif
+	u_int8_t	tos;
+	u_int16_t	tlen;
+	u_int16_t	id;
+	u_int16_t	offs;
+	u_int8_t	ttl;
+	u_int8_t	proto;
+	u_int16_t	cksum;
+	u_int32_t	saddr;
+	u_int32_t	daddr;
+};
+
+struct ip_header_v6 {
+#if defined(__LITTLE_ENDIAN__)
+        u_int8_t	prioHi:4, ver:4;
+#else
+        u_int8_t	ver:4, prioHi:4;
+#endif
+#if defined(__LITTLE_ENDIAN__)
+        u_int8_t	zero:4, prioLo:4;
+#else
+        u_int8_t	prioLo:4, zero:4;
+#endif
+	u_int16_t	label; 
+	u_int16_t	pllen;
+	u_int8_t	nexthdr;
+	u_int8_t	hoplim;
+	u_int8_t	saddr[IPv6_ADDRESS_SIZE];
+	u_int8_t	daddr[IPv6_ADDRESS_SIZE];
+};
+
+/*
+ * Type definitions
+ */
+typedef struct udp_header udp_hdr;
 typedef struct tcp_header tcp_hdr;
+typedef struct ip_header_v4 ipv4_hdr;
+typedef struct ip_header_v6 ipv6_hdr;
 
-#define TCP_SIZE (20)
 
+__inline__ void ipv4_swap(ipv4_hdr *ih)
+{	
+	u_int32_t swap;
+	
+	swap = ih->daddr;
+	
+	ih->daddr = ih->saddr;
+	ih->saddr = swap;
+}
+
+__inline__ void ipv6_swap(ipv6_hdr *ih)
+{	
+	u_int8_t swap[16];
+	
+	memcpy(&swap[0], &ih->daddr[0], 16);
+	memcpy(&ih->daddr[0], &ih->saddr[0], 16);
+	memcpy(&ih->saddr[0], &swap[0], 16);
+}
