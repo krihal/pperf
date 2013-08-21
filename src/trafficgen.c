00273531662f3895bdab47f353c447fdb9737a32
Kristofer Hallin <krsitofer.hallin@gmail.com>
2013-08-21 11:16:07 +0200

    IPv4 header assembler

--- src/trafficgen.c
+++ src/trafficgen.c
@@ -7,46 +7,76 @@
 #include <unistd.h>
 #include <netdb.h>
 
-/*
- * Swap source and destination ports before returning packet
- */
-
-__inline__ void udp_swap_ports(udp_hdr *udp)
-{
-	u_int16_t swap;
-
-	swap = udp->dport;
-	udp->dport = udp->sport;
-	udp->sport = swap;
-}
-
-__inline__ void tcp_swap_ports(tcp_hdr *tcp)
-{
-	u_int16_t swap;
-
-	swap = tcpp->dport;
-	tcp->dport = udp->sport;
-	tcp->sport = swap;
-}
+#include "trafficgen.h"
 
 /* 
  * UDP checksum calculator 
  */
-u_int32_t checksum(unsigned char *buf, unsigned nbytes, u_int32_t sum)
+unsigned short ip_checksum(unsigned short *ptr, int nbytes) 
 {
-	int i;
-
-	for (i = 0; i < (nbytes & ~1U); i += 2) {
-	   	sum += (u_int16_t)ntohs(*((u_int16_t *)(buf + i)));
-		if (sum > 0xFFFF)
-			sum -= 0xFFFF;
+	register long sum;
+	unsigned short oddbyte;
+	register short answer;
+ 
+	sum=0;
+	while(nbytes>1) {
+		sum+=*ptr++;
+		nbytes-=2;
 	}
 
-	if (i < nbytes) {
-		sum += buf[i] << 8;
-		if (sum > 0xFFFF)
-			sum -= 0xFFFF;
+	if(nbytes==1) {
+		oddbyte=0;
+		*((u_char*)&oddbyte)=*(u_char*)ptr;
+		sum+=oddbyte;
 	}
+	
+	sum = (sum>>16)+(sum & 0xffff);
+	sum = sum + (sum>>16);
+	answer=(short)~sum;
+	
+	return(answer);
+}
+ 
+ipv4_hdr ip4_packet(ip_packet_control *ipc)
+{
+	ipv4_hdr ip;
+	uint32_t total_length = 0;		
+	struct sockaddr_storage ss_dst;
+	struct sockaddr_in *sa_dst;
 
-	return (sum);
+	ss_dst = ipc->dst;
+	sa_dst = (struct sockaddr_in *)&ss_dst;
+
+	ip.hlen = 5;
+	ip.ver = 4;
+	ip.tos = ipc->tos;
+	ip.tlen = total_length;
+	ip.id = htonl(1234);
+	ip.offs = 0;
+	ip.ttl = ipc->ttl;
+	ip.proto = ipc->hdr_type;
+	ip.cksum = 0;
+	ip.saddr = inet_addr(ipc->source_ip);
+	ip.daddr = sa_dst->sin_addr.s_addr;
+	ip.cksum = ip_checksum((unsigned short *)&ip, ip.hlen);
+
+	return ip;
+
+}
+
+ipv6_hdr ip6_packet(ip_packet_control *ipc)
+{
+	ipv6_hdr ip;
+
+	return ip;
+}
+
+char *packet_udp()
+{
+	return NULL;
+}
+
+char *packet_tcp() 
+{
+	return NULL;
 }
